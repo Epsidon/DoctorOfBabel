6821c66912acbb20844841272d845622f749536a
Alaz Gulec <alazgulec@gmail.com>
2014-10-11 17:58:32 -0400

    Ajax and some UI work

--- babelserver/public/ajax.js
+++ babelserver/public/ajax.js
@@ -1,12 +1,16 @@
-function populateExpressions(languageId){
-	var requestURI = "/api/languages/" + languageId;
+function populateExpressions(langId){
+	var requestURI = "/api/languages/" + langId;
 	$.get(requestURI, function(exprs, status){
     	var contextBuilder = "";
-    	var divStart = "<div>"
-    	var divEnd = "</div>"
     	exprs.forEach(function(expr) {
+    	var outerCover = '<div id="' + expr._id + '" class="expression">' 
+    		    	+ '<div>';
+    	var innerCover = '</div><audio controls><source src="' + this.audio + '" type="audio/mpeg">' 
+    	+ 'Your browser does not support the audio element.' 
+    	+ '</audio>' 
+    	+ '</div>';
     		contextBuilder = contextBuilder +
-    			divStart + expr.english + " - " + expr.translation + divEnd;
+    			outerCover + expr.english + " - " + expr.translation + innerCover;
     			$("#expressionList").html(contextBuilder);
     	});
   	});
