2d1dd79997285d85388ec17f5599a5c613633ec6
yousifh <yaseryousif@gmail.com>
2014-10-19 16:19:05 -0400

    Current work on signup functionality

--- babelserver/config/auth.js
+++ babelserver/config/auth.js
@@ -2,7 +2,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
 
 module.exports = function(passport) {
-	passport.use(new LocalStrategy(
+	passport.use('login', new LocalStrategy(
 		function(username, password, done) {
 			User.findOne({ username: username }, function(err, user) {
 				if (err)
@@ -15,9 +15,33 @@ module.exports = function(passport) {
 			});
 		}
 	));
+
+	passport.use('signup', new LocalStrategy({
+			passReqToCallback: true
+		},
+		function(req, username, password, done) {
+			User.findOne({ username: username}, function(err, user) {
+				if (err)
+					return done(err);
+				if(user)
+					return done(null, false, { message: 'User already exists'});
+				// User doesn't exist
+				var newUser = new User();
+				user.username = req.params.username;
+				user.password = req.params.password;
+				user.role = 'normal';
+
+				newUser.save(function(err) {
+					if (err)
+						console.log(err);
+					return(null, newUser);
+				});
+			});
+		}
+	));
 	
 	passport.serializeUser(function(user, done) {
-		done(null, user.id);
+		done(null, user._id);
 	});
 
 	passport.deserializeUser(function(id, done) {
