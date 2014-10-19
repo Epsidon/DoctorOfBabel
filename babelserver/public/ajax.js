e4b8abe74419fc569936e0950123e668a35c7047
Alaz Gulec <alazgulec@gmail.com>
2014-10-18 21:55:17 -0400

    This patch aims to make UI more mobile responsive.
    Exceptional:
    Back mechanism is still buggy.

--- babelserver/public/ajax.js
+++ babelserver/public/ajax.js
@@ -1,10 +1,44 @@
-function populateExpressions(langId, info, mapPath){
+var mIsMobileBrowser = false;
+var el = document.getElementById("expression-list");
+var backButton = document.getElementById("back-icon");
+
+ // executes when HTML-Document is loaded and DOM is ready
+$(document).ready(function() {
+    isMobileBrowser();
+});
+
+
+//Captures width resize
+$(window).resize(function() {
+    isMobileBrowser();
+});
+
+function isMobileBrowser(width) {
+    
+    //Bootstrap's XS param covering screen sizes smaller than 768px;
+    var bootstrapXsParam = 768;
+    if($(window).width() <= bootstrapXsParam) {
+        mIsMobileBrowser = true;
+    } else {
+        mIsMobileBrowser = false;
+            el.style.visibility = "visible";
+
+    }
+}
+
+function closeModal() {
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
+function populateExpressions(langId, info, mapPath) {
     $("#info").text(info);
     $("#map").attr('src', mapPath);
+    var backButton = '<img id = "back-icon" src="/images/back-icon.png" onClick="closeModal();">';
+    var contextBuilder = backButton;
 	var requestURI = "/api/languages/" + langId;
 	$.get(requestURI, function(exprs, status){
         if (exprs.length != 0) {
-    	   var contextBuilder = "";
+
     	   exprs.forEach(function(expr) {
     	   var outerCover = '<div id="' + expr._id + '" class="expression">' 
     		    	+ '<div>';
@@ -14,8 +48,11 @@ function populateExpressions(langId, info, mapPath){
     	   + '</div>';
     		contextBuilder = contextBuilder +
     			outerCover + expr.english + " - " + expr.translation + innerCover;
-    			$("#expressionList").html(contextBuilder);
+    			$("#expression-list").html(contextBuilder);
     	   });
+            if(mIsMobileBrowser) {
+                closeModal();
+            }
         }
   	});
 }
