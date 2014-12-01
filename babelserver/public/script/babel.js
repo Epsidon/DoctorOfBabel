dd17c5a565320a249777f8f0914642a0e5bcca69
yousifh <yaseryousif@gmail.com>
2014-12-01 08:50:24 -0500

    UI Revamp so it uses Twitter Bootstrap grid system in easier way to understand and debug.

--- babelserver/public/script/babel.js
+++ babelserver/public/script/babel.js
@@ -16,49 +16,53 @@ var bootstrapXsParam = 768;
 });*/
 
 function isMobileBrowser(width) {
-    if($(window).width() <= bootstrapXsParam) {
-        mIsMobileBrowser = true;
-        $('#hidden-header').show();
-        return true;
-    } else {
-        mIsMobileBrowser = false;
-        el.style.visibility = "visible";
-        $('#hidden-header').hide();
-        return false;
-    }
+  if($(window).width() <= bootstrapXsParam) {
+    mIsMobileBrowser = true;
+    $('#hidden-header').show();
+    return true;
+  } else {
+    mIsMobileBrowser = false;
+    el.style.visibility = "visible";
+    $('#hidden-header').hide();
+    return false;
+  }
 }
 
 function closeModal() {
-    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+  el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
 
 function populateExpressions(langId, name, info, mapPath) {
-    $("#info").text(info);
-    $("#map").attr('src', mapPath);
-    var hiddenHeader = '<div id="hidden-header">' +
-    '<img style="float:left;"id="back-icon" src="/images/back-icon.png" onClick="closeModal();">' + 
-    '<h4 style="text-align: center;">' + name + '</h4>' +
-    '</div>';
-    var contextBuilder = hiddenHeader;
-    var requestURI = "/api/languages/" + langId;
-    $.get(requestURI, function(exprs, status){
-        if (exprs.length != 0) {
-
-           exprs.forEach(function(expr) {
-           var outerCover = '<div id="' + expr._id + '" class="expression">' 
-                    + '<div>';
-           var innerCover = '</div><audio controls style="width:100%;"><source src="' + this.audio + '" type="audio/mpeg">' 
-           + 'Your browser does not support the audio element.' 
-           + '</audio>' 
-           + '</div>';
-            contextBuilder = contextBuilder +
-                outerCover + expr.english + " - " + expr.translation + innerCover;
-                $("#expression-list").html(contextBuilder);
-           });
-            if(isMobileBrowser()) {
-                closeModal();
-            }
-        }
-    });
+  $('#display-title').remove();
+  $('#expr-title').removeClass('hidden');
+  $('#info-title').removeClass('hidden');
+  $('#expression-list').removeClass('hidden');
+  $('#info-container').removeClass('hidden');
+  $("#info").text(info);
+  $("#map").attr('src', mapPath);
+  var hiddenHeader = '<div id="hidden-header">' +
+  '<img style="float:left;"id="back-icon" src="/images/back-icon.png" onClick="closeModal();">' + 
+  '<h4 style="text-align: center;">' + name + '</h4>' +
+  '</div>';
+  var contextBuilder = hiddenHeader;
+  var requestURI = "/api/languages/" + langId;
+  $.get(requestURI, function(exprs, status) {
+    if (exprs.length != 0) {
+      exprs.forEach(function(expr) {
+        var outerCover = '<div id="' + expr._id + '" class="expression">' 
+                + '<div>';
+        var innerCover = '</div><audio controls style="width:100%;"><source src="/' + expr.audio + '" type="audio/mpeg">' 
+        + 'Your browser does not support the audio element.' 
+        + '</audio>' 
+        + '</div>';
+        contextBuilder = contextBuilder +
+            outerCover + expr.english + " - " + expr.translation + innerCover;
+            $("#expression-list").html(contextBuilder);
+      });
+      if(isMobileBrowser()) {
+        closeModal();
+      }
+    }
+  });
 }
 
